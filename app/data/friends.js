77ff849dd1213b367e4ae6323ffd3afbc0c8729c
Jason Finney <finney.jason.218@gmail.com>
2018-09-11 21:53:21 -0500

    almost done

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendsArray = [
+    {
+        friendName: "Jason",
+        friendPhoto: "exampleurl.jpg",
+        question1: "1",
+        question2: "1",
+        question3: "1",
+        question4: "1",
+        question5: "1",
+        question6: "1",
+        question7: "1",
+        question8: "1",
+        question9: "1",
+        question10: "1"
+    }
+];
+
+module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 9000;
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require("./routing/htmlRoutes")(app);
+require("./routing/apiRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
